4899cab0088e33b3cf0e8d21f39a510bd3610786
mpuchyr <matt.puchyr@gmail.com>
2022-10-20 15:39:46 +0000

    Create route and method in posts_controller for new_post_form.html.erb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,8 @@ class PostsController < ApplicationController
   def index
     render({:template => "/post_templates/index.html.erb"})
   end
+
+  def new
+    render({:template => "/post_templates/new_post_form.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   get("/", {:controller => "posts", :action => "index"})
 
+  get("/posts/new", {:controller => "posts", :action => "new"})
+
 
 end
